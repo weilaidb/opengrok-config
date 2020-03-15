82bc6bcc2b8cfc7a1a9c67b836f0fccffaa140fa
CentOS-7-X86_64 <duo@localhost.localdomain>
2020-03-15 21:22:03 +0800

    gen opengrok project for linux sh

---
+++ gen_opengrok_prj_linux.sh
@@ -0,0 +1,2 @@
+java -Xms524m -jar /home/duo/soft/opengrok/opengrok-1.3.3/lib/opengrok.jar -W "/home/duo/soft/opengrok/opengrok-1.3.3/lib/configuration.xml" -c /usr/local/bin/ctags -P -S -v -s "/home/duo/soft/opengrok/project" -d "/home/duo/soft/opengrok/data"
+
